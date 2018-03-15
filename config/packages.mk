cd53f77cd59b73594169a896f8874cbafbb3e132
Alex Cruz <du.alexcruz@gmail.com>
2018-03-15 01:42:23 -0400

    theme back
    Change-Id: Ie2cc7f00aa72416882c518ff4512204daef8e3fd

--- config/packages.mk
+++ config/packages.mk
@@ -1,7 +1,45 @@
 # Packages
 PRODUCT_PACKAGES += \
-    WallpaperPicker \
-    WeatherClient
+    Turbo
+
+# Overlays
+PRODUCT_PACKAGES += \
+    AmberAccent \
+    BlackAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    DuiDark \
+    GBoardDark \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    SettingsDark \
+    SystemDark \
+    TealAccent \
+    YellowAccent \
+    WhiteAccent
+
+# Fonts
+PRODUCT_PACKAGES += \
+    DU-Fonts
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
